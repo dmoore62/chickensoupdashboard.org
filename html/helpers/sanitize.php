8777e69ed80745ca1cd5a2c499770b16eb8284f6
Daniel Moore <dan@digitaldocs.net>
2014-03-06 01:35:19 -0500

    fixes live errors

--- html/helpers/sanitize.php
+++ html/helpers/sanitize.php
@@ -1,6 +1,8 @@
 <?
 function sanitize($in){
-	$out = mysql_real_escape_string($in);
+	//later this will work with the DB loaded
+	//$out = mysql_real_escape_string($in);
+	$out = $in;
 	return $out;
 }
 ?>
