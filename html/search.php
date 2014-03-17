f2e6e270c63cad03c23018aa49f2f02aff8356f5
Chris Mayhew <chrismayhew@knights.ucf.edu>
2014-03-17 11:48:14 -0400

    Update search.php
    There needed a separate between the heading for recipient and volunteers so I moved them up to the 1st list item.

--- html/search.php
+++ html/search.php
@@ -5,17 +5,15 @@ $term = $_GET['term'];
 //sql to search vols
 ?>
 <ul id="out-list">
-	<li>
+	<li>Recipients
 		<ul id="recip-list">
-			<li>Recipients</li>
 			<li><?php echo $term;?></li>
 			<li>TEST recip 2</li>
 			<li>recip, test</li>
 		</ul>
 	</li>
-	<li>
+	<li>Volunteers
 		<ul id="vol-list">
-			<li>Volunteers</li>
 			<li>TEST volenteer 1</li>
 			<li>TEST volenteer 2</li>
 			<li>volenteer, test</li>
