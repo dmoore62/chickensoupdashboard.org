fbe98ff04209a26d1385d645a221b4ba82ca4a8f
Chris Mayhew <chrismayhew@knights.ucf.edu>
2014-03-31 14:56:09 -0400

    Update view.php
    Added volunteer availability and added values to general view/update form.

--- html/volunteers/view.php
+++ html/volunteers/view.php
@@ -5,13 +5,39 @@ if($_SESSION["logged_in"] != true){
 //die(var_dump($_POST));
 
 $vid = $_GET['vid'];
-//check for new vol and do insert and get insert id
+$dowMap = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+if($vid && isset($_POST['availability'])){
+	if($_POST['new']){
+		for($i=0; $i<7; $i++){
+			$amv = $dowMap[$i]."a";
+			$pmv = $dowMap[$i]."p";
+			if(isset($_POST[$amv])) $am = 1; else $am = 0;
+			if(isset($_POST[$pmv])) $pm = 1; else $pm = 0;
+			$insert_sql = "INSERT into VAvailable (weekday, AM, PM, VID) VALUES ($i, $am, $pm, $vid);";
+			$rresult = mysql_query($insert_sql) or die(mysql_error());
+		}
+	}else{
+		for($i=0; $i<7; $i++){
+			$amv = $dowMap[$i]."a";
+			$pmv = $dowMap[$i]."p";
+			if(isset($_POST[$amv])) $am = 1; else $am = 0;
+			if(isset($_POST[$pmv])) $pm = 1; else $pm = 0;
+			$update_sql = "UPDATE VAvailable SET AM=$am, PM=$pm WHERE weekday = $i AND VID = $vid;";
+			$rresult = mysql_query($update_sql) or die(mysql_error());
+		}
+	}
+}
+
+$select_sql = "SELECT * FROM Volunteers WHERE VID = '$vid';";
+$result = mysql_query($select_sql);
+$v = mysql_fetch_assoc($result);
+
+$select_sql2 = "SELECT * FROM VAvailable WHERE VID = '$vid' ORDER BY weekday ASC;";
+$result2 = mysql_query($select_sql2);
+$va = mysql_fetch_assoc($result2);
 if(!$vid && $_POST){
 
 }
-
-//use $vid to get vol record
-
 
 //volunteer/search controller
 //set css and js for this page
@@ -42,55 +68,37 @@ $active = 'volunteer'
 							<div class="control-group">  
 					            <label class="control-label" for="fname">First Name</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large" name="fname" id="fname">  
+					              <input type="text" class="input-large" name="fname" id="fname" value="<?php echo $v['first_name'];?>">  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="lname">Last Name</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large" name="lname" id="lname">  
+					              <input type="text" class="input-large" name="lname" id="lname" value="<?php echo $v['last_name'];?>">  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="email">E-Mail</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large" name="email" id="email">  
+					              <input type="text" class="input-large" name="email" id="email" value="<?php echo $v['email'];?>">  
 					            </div>  
 					        </div>
-					        <div class="control-group">  
-					            <label class="control-label" for="address">Address</label>  
-					            <div class="controls">  
-					              <input type="text" class="input-large" name="address" id="address">  
-					            </div>  
-					        </div>
-					        <div class="control-group">  
-					            <label class="control-label" for="city">City</label>  
-					            <div class="controls">  
-					              <input type="text" class="input-large" name="city" id="city">  
-					            </div>  
-					        </div>
-					        <div class="control-group">  
-					            <label class="control-label" for="postal">Zip</label>  
-					            <div class="controls">  
-					              <input type="text" class="input-large" name="postal" id="postal">  
-					            </div>  
-					        </div> 
-							<div class="control-group">  
+						<div class="control-group">  
 					            <label class="control-label" for="hphone">Home Phone</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large phone-mask" name="hphone" id="hphone">  
+					              <input type="text" class="input-large phone-mask" name="hphone" id="hphone" value="<?php echo $v['home_phone'];?>">  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="cphone">Cell Phone</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large phone-mask" name="cphone" id="cphone">  
+					              <input type="text" class="input-large phone-mask" name="cphone" id="cphone" value="<?php echo $v['cell_phone'];?>">  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="wphone">Work Phone</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large phone-mask" name="wphone" id="wphone">  
+					              <input type="text" class="input-large phone-mask" name="wphone" id="wphone" value="<?php echo $v['work_phone'];?>">  
 					            </div>  
 					        </div>
 				        </div>
@@ -100,8 +108,8 @@ $active = 'volunteer'
 					            <label class="control-label" for="suv">Has SUV?</label>  
 					            <div class="controls">  
 					              <select name="suv" id="suv" class="form-control">
-					              	<option value="1">Yes</option>
-					              	<option value="0">No</option>
+					              	<option value="1" <?php if($v['SUV']) echo "selected";?>>Yes</option>
+					              	<option value="0" <?php if(!$v['SUV']) echo "selected";?>>No</option>
 					              </select>  
 					            </div>  
 					        </div>
@@ -109,8 +117,8 @@ $active = 'volunteer'
 					            <label class="control-label" for="meals">Meals?</label>  
 					            <div class="controls">  
 					              <select name="meals" id="meals" class="form-control">
-					              	<option value="1">Yes</option>
-					              	<option value="0">No</option>
+					              	<option value="1" <?php if($v['meal']) echo "selected";?>>Yes</option>
+					              	<option value="0" <?php if(!$v['meal']) echo "selected";?>>No</option>
 					              </select>  
 					            </div>  
 					        </div>
@@ -118,8 +126,8 @@ $active = 'volunteer'
 					            <label class="control-label" for="transport">Transport?</label>  
 					            <div class="controls">  
 					              <select name="transport" id="transport" class="form-control">
-					              	<option value="1">Yes</option>
-					              	<option value="0">No</option>
+					              	<option value="1" <?php if($v['trans']) echo "selected";?>>Yes</option>
+					              	<option value="0" <?php if(!$v['trans']) echo "selected";?>>No</option>
 					              </select>  
 					            </div>  
 					        </div>
@@ -127,8 +135,8 @@ $active = 'volunteer'
 					            <label class="control-label" for="visits">Visits?</label>  
 					            <div class="controls">  
 					              <select name="visits" id="visits" class="form-control">
-					              	<option value="1">Yes</option>
-					              	<option value="0">No</option>
+					              	<option value="1" <?php if($v['visit']) echo "selected";?>>Yes</option>
+					              	<option value="0" <?php if(!$v['visit']) echo "selected";?>>No</option>
 					              </select>  
 					            </div>  
 					        </div>
@@ -136,27 +144,27 @@ $active = 'volunteer'
 					            <label class="control-label" for="errands">Errands?</label>  
 					            <div class="controls">  
 					              <select name="errands" id="errands" class="form-control">
-					              	<option value="1">Yes</option>
-					              	<option value="0">No</option>
+					              	<option value="1" <?php if(!$v['errands']) echo "selected";?>>Yes</option>
+					              	<option value="0" <?php if(!$v['']) echo "selected";?>>No</option>
 					              </select>  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="activities">Other Activities</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large phone-mask" name="activities" id="activities">  
+					              <input type="text" class="input-large phone-mask" name="activities" id="activities" value="<?php echo $v['other_acts'];?>">  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="secLang">2nd Language</label>  
 					            <div class="controls">  
-					              <input type="text" class="input-large" name="secLang" id="secLang">  
+					              <input type="text" class="input-large" name="secLang" id="secLang" value="<?php echo $v['sencond_lang'];?>">  
 					            </div>  
 					        </div>
 					        <div class="control-group">  
 					            <label class="control-label" for="notes">Notes</label>  
 					            <div class="controls">  
-					              <textarea rows="2" class="input-large" name="notes" id="notes"></textarea>
+					              <textarea rows="2" class="input-large" name="notes" id="notes"><?php echo $v['comments'];?></textarea>
 					            </div>  
 					        </div>
 				        </div>
@@ -174,8 +182,40 @@ $active = 'volunteer'
 		<div class="vol-view" id="availablity">
 			<div class="tab-content-wrapper">
 				<h4>Availablity</h4>
-				<form name="vol_avail" action="avail_post.php" method="POST">
-				
+				<?php if(isset($_POST['availabilty'])){
+					if($rresult){ ?>
+						<div class="alert alert-success">Availabilty updated successfully!</div>
+				<?php 	}else{ ?>
+						<div class="alert alert-error">Availabilty did not update successfully!</div>
+				<?php }} ?>
+				<form name="vol_avail" method="post">
+					<table class="table table-striped table-hover">
+						<thead><tr>
+							<th></th>
+							<th>AM</th>
+							<th>PM</th>
+						</tr></thead>
+						<?php 
+						if(mysql_num_rows($result2)>0){ ?>
+							<input type="hidden" name="new" value="0">
+							<?php do{ ?>
+							<tr>
+								<th><?php echo $dowMap[$va['weekday']];?></th>
+								<td><input type="checkbox" class="input-large" name="<?php echo $dowMap[$va['weekday']];?>a" <?php if($va['AM']) echo "checked"; ?>></td>
+								<td><input type="checkbox" class="input-large" name="<?php echo $dowMap[$va['weekday']];?>p" <?php if($va['PM']) echo "checked"; ?>></td>
+							</tr>
+						<?php }while($va = mysql_fetch_assoc($result2)); 
+						}else{ ?>
+							<input type="hidden" name="new" value="1">
+							<?php for($i = 0; $i<7; $i++){?>
+								<tr>
+									<th><?php echo $dowMap[$i];?></th>
+									<td><input type="checkbox" class="input-large" name="<?php echo $dowMap[$i];?>a"></td>
+									<td><input type="checkbox" class="input-large" name="<?php echo $dowMap[$i];?>p"></td>
+								</tr>
+						<?php }} ?>
+					</table>
+					<button type="submit" class="btn btn-primary" name="availability" value="availability">Update Availability</button>
 				</form>
 			</div>
 		</div>
