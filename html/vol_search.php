c374d1ce568dfeeed91498a59a1cead5d4496f97
Daniel Moore <dan@digitaldocs.net>
2014-04-01 14:12:11 -0400

    adds vol search modal

--- html/vol_search.php
+++ html/vol_search.php
@@ -1,48 +1,57 @@
-<?php session_start();
-if($_SESSION["logged_in"] != true){
-    header("Location: http://".$_SERVER["HTTP_HOST"]);
-}
+<?php
 
-require "config/mysql_header.php";
+require "../config/mysql_header.php";
 
-$eid = $_GET['EID'];
+$eid = $_GET['eid'];
 //welcome controller
 //set css and js for this page
-$stylesheet = '/css/welcome.css';
-$script = '/js/welcome.js';
-$active = 'dashboard';
+
 $select_sql = "SELECT * FROM Events WHERE EID = '$eid';";
 $results = mysql_query($select_sql);
 $e = mysql_fetch_assoc($results);
 $wday = date("w", strtotime($e['start_date']));
 if(date("a", strtotime($e['arrive_time']))=="am"){ $am = 1; $pm = 0; }else{ $am = 0; $pm = 1; }
 
-$select_sql = "SELECT * FROM Volunteers V, VAvailable B WHERE V.VID = B.VID AND B.weekday = '$wday' AND B.AM = '$am' AND B.PM = '$pm' ORDER BY V.last_name ASC;";
+//$select_sql = "SELECT * FROM Volunteers V, VAvailable B WHERE V.VID = B.VID AND B.weekday = '$wday' AND B.AM = '$am' AND B.PM = '$pm' ORDER BY V.last_name ASC;";
+$select_sql = "SELECT * FROM Volunteers;";
 $result = mysql_query($select_sql);
-$v = mysql_fetch_assoc($result);
+//$v = mysql_fetch_assoc($result);
 
 ?>
-<?php require "header.php";?>
-<?php require "nav.php";?>
-<div id="page-content">
-	<div id="nav-wrapper">
-	<p>You are currently viewing available volunteers on <?php echo $e['start_date']." at ".date("h:i:s A", strtotime($e['arrive_time'])); ?>.</p>
+<div class="modal" id="vol-modal">
+	<div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Available Volunteers</h4>
+    </div>
+	<div id="modal-body">
+		<div class="content-wrapper">
+		<!-- <p>You are currently viewing available volunteers on <?php echo $e['start_date']." at ".date("h:i:s A", strtotime($e['arrive_time'])); ?>.</p> -->
+		<div class="table-wrapper">
+			<table class="dynamic-styled">
+				<thead>
+					<tr>
+						<th>Volunteer Name</th>
+						<th>Last Event</th>
+						<th>Called?</th>
+						<th>Add to Event</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php while($v= mysql_fetch_assoc($result)): ?>
+						<tr>
+							<td><?php echo $v['last_name'].", ".$v['first_name']; ?></td>
+							<td>4/01/2014</td>
+							<td><input type="checkbox" name="called" value="checked" checked/></td>
+							<td><button type="button" class="btn btn-primary" name="call">Add to Event</button></td>
+						</tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+		</div>
 	</div>
-	<div id="content-wrapper">
-	<h4>Available Volunteers</h4>
-		<table class="table table-striped">
-			<thead><tr>
-				<th>Volunteer Name</th>
-				<th></th>
-			</tr></thead>
-			<?php do{ ?>
-				<tr>
-					<td><?php echo $v['last_name'].", ".$v['first_name']; ?></td>
-					<td><button type="button" class="btn btn-primary" name="call">Called</button></td>
-				</tr>
-			<?php }while($v = mysql_fetch_assoc($result)); ?>
-		</table>
-	</div>
+	<div class="modal-footer"></div>
 </div>
-
-<?php require "footer.php";?>
+<script type="text/javascript">
+	$('table.dynamic-styled').dataTable();
+</script>
