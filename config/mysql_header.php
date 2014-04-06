c753a12333d58998e37e5701df8da599da896fdf
Daniel Moore <dan@digitaldocs.net>
2014-04-06 10:35:00 -0400

    try this:

--- config/mysql_header.php
+++ config/mysql_header.php
@@ -1,7 +1,7 @@
 <?php
 	$user = "root";
 	$password = "root";
-	$database = "dbchickensoup";
+	$database = "db173908_chickensoup";
 	mysql_connect("localhost", $user, $password) or die("ERROR on DB connect");
 	@mysql_select_db($database) or die("Unable to select db"); 
 ?>
